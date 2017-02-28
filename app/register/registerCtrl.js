3815379779999d495e0b49dd792b1023ac742fd6
kumaranish8888 <kumaranish8888@gmail.com>
2017-02-28 16:44:16 -0500

    Files modified

--- app/register/registerCtrl.js
+++ app/register/registerCtrl.js
@@ -2,23 +2,23 @@
 
     function registerCtrl(registerSvc) {
         var vm = this;
-        vm.countries = registerSvc.getCountries();
+       // vm.countries = registerSvc.getCountries();
         //make a call to the register service
         //fetch the data from the json
-    /*    registerSvc.getCountriesFromJson()
-        //handle positive response
-            .then(function (response) {
-                console.log(response);
-                vm.countries = response.data.countries;
-            })
-            //handle negative response
-            .catch(function (response) {
-                console.log(response);
-            })
-            // handle everything.
-            .finally(function (response) {
-                console.log(response);
-            }); */
+       registerSvc.getCountriesFromJson()
+       .then(function(response){
+           console.log(response);
+           vm.countries = response.data.countries;
+       })
+       
+       .catch(function(response){
+           
+       })
+       
+       .finally(function(){
+           
+       });
+        
 
             
 
--- app/register/registerSvc.js
+++ app/register/registerSvc.js
@@ -1,14 +1,20 @@
 (function(){
     
-    angular.module("register").service("registerSvc", [registerSvc]);
+    angular.module("register").service("registerSvc", ["$http", registerSvc]);
     
-    function registerSvc(){
+    function registerSvc($http){
         
         this.getCountries = function(){
             return [
                 {"name": "India", "code": "IN"},
                 {"name": "United States", "code": "USA"}
             ];
+        };
+        
+        this.getCountriesFromJson = function(){
+            
+            return $http.get("api/country.json");
+        
         };
         
     };
