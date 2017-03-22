6cd9010c745f08c91c1704f764a755fffa84cdd6
kumaranish8888 <kumaranish8888@gmail.com>
2017-03-22 14:11:05 -0400

    modified datepicker

--- app/common/common.js
+++ app/common/common.js
@@ -1,7 +1,6 @@
 (function(){
     
     angular.module("common",[]);
-    
     angular.module("common").directive("firstDir", [firstDir]);
     
     function firstDir(){
@@ -16,6 +15,37 @@
         return {
             templateUrl: 'app/header/header.tpl.html',
             restrict: "A"
+        };
+    }
+    
+    angular.module("common").directive("numbersOnly", [numbersOnly]);
+    
+    function numbersOnly(){
+        
+        return{
+          restrict:"A",
+            link: function(scope,element, attrs){
+                element.bind("keypress", function(e){
+                    var selected = e.key;
+                    var reg = new RegExp(/^[0-9]+$/);
+                    var maxlength = attrs[maxlength];
+                    if(!reg.test(selected) && this.value.length > maxlength){
+                        e.preventDefault();
+                    }
+                });
+            }
+        };
+        
+    }
+    
+    angular.module("common").directive("customDatePicker", [customDatePicker]);
+    
+    function customDatePicker(){
+        return {
+          restrict: "A",
+            link: function(scope,element, attrs){
+                element.datepicker();
+            }
         };
     }
     
